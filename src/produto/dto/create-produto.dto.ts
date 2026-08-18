fa942a7320ffda83c6fe5f9bd7c8d21042559b85
João Pedro Senai-B04 <joao.p.silva662@aluno.senai.br>
2026-08-18 16:50:44 -0300

    Criação da API

---
+++ src/produto/dto/create-produto.dto.ts
@@ -0,0 +1,23 @@
+import { IsBoolean, IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Min } from "class-validator";
+
+export class CreateProdutoDto {
+    @IsString()
+    @IsNotEmpty()
+    nome: string;
+
+    @IsString()
+    @IsOptional()
+    descreicao?: string;
+
+    @IsNumber()
+    @IsPositive()
+    preco: number;
+
+    @IsInt()
+    @Min(0)
+    quantidade: number;
+
+    @IsBoolean()
+    @IsOptional()
+    status?: boolean
+}
